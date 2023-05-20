ae1046a39b468e33182d610bd0c1739482f52420
Avneet Singh <avneetmaankiya@gmail.com>
2023-05-21 05:22:22 +0530

    paging

--- app/src/main/java/com/avicodes/halchalin/presentation/ui/home/reports/remote/RemoteNewsAdapter.kt
+++ app/src/main/java/com/avicodes/halchalin/presentation/ui/home/reports/remote/RemoteNewsAdapter.kt
@@ -1,19 +1,13 @@
 package com.avicodes.halchalin.presentation.ui.home.reports.remote
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.avicodes.halchalin.R
-import com.avicodes.halchalin.data.models.News
 import com.avicodes.halchalin.data.models.NewsRemote
-import com.avicodes.halchalin.databinding.ItemLoadMoreBinding
 import com.avicodes.halchalin.databinding.ItemRemoteNewsBinding
-import com.avicodes.halchalin.presentation.ui.home.home.CategoryNewsAdapter
 import com.bumptech.glide.Glide
 
 
